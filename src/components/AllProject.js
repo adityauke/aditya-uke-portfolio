aff8f3a5a7971d46a298afab3247a6523e883a9d
Aditya Uke <adityauke4141@gmail.com>
2023-07-05 22:24:57 +0530

    Update AllProject.js

--- src/components/AllProject.js
+++ src/components/AllProject.js
@@ -13,16 +13,16 @@ function AllProject() {
         }
 
         return (
-          <div className="container text-center my-5 py-5">
+          <div className="container-fluid bg-light text-center mt-5 py-5">
             <div>
               <h1 className="font-weight-light">
                 All my <span className="text-info">Projects</span>
               </h1>
               <div className="lead pb-4">Crafted with Dedication: Showcasing My Project Achievements</div>
             </div>
-            <div className="row my-4 pt-4">
+            <div className="row my-2 ">
             {value.projects.map((project, index) => (
-            <div key={index} className="col-12 col-md-6 py-3">
+            <div key={index} className=" py-3">
               <ProjectCard
                 title={project.title}
                 description={project.description}
